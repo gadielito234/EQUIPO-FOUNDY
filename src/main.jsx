84383fb70d49740eccb70ce5f651ca63b9c8a7cc
rubisita-brb <rivas.sandoval2027@hilasal.superate.org>
2026-08-16 04:19:00 -0600

    home diseñado

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+import "bootstrap-icons/font/bootstrap-icons.css";
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
--- src/pages/inicio.jsx
+++ src/pages/inicio.jsx
@@ -1,24 +1,12 @@
+import Home from './Home';
+
 function Inicio({ usuarioData, onCerrarSesion }) {
     return (
-        <div className="min-vh-100 d-flex justify-content-center align-items-center bg-light">
-            <div className="card shadow p-4 text-center" style={{ width: "400px" }}>
-                <h2 className="fw-bold mb-2">
-                    ¡Bienvenido, {usuarioData.nombre} Ahora eres parte de la familia FOUNDY!
-                </h2>
-                <p className="text-muted mb-4">
-                    @{usuarioData.usuario} ({usuarioData.nombre} {usuarioData.apellidos})
-                </p>
-                <div className="alert alert-success py-2 small mb-4" role="alert">
-                    Has iniciado sesión correctamente.
-                </div>
-                <button
-                    className="btn btn-outline-danger w-100"
-                    onClick={onCerrarSesion}
-                >
-                    Cerrar sesión
-                </button>
-            </div>
-        </div>
+        <Home
+            usuarioData={usuarioData}
+            onCerrarSesion={onCerrarSesion}
+        />
     );
 }
+
 export default Inicio;
